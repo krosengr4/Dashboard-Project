226360bcf4a7d7379d0d3c379555163102a391ab
krosengr4 <rosenkev4@gmail.com>
2024-02-25 21:45:55 -0600

    aded homepage.jsx and started coding

---
+++ client/src/pages/Home-Page.jsx
@@ -0,0 +1,12 @@
+//! This file contains the homepage of the dashboard. 
+
+import '../app.css';
+
+export default function homePage() {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
