f41bd11a95b93fe5087dd838c792937161a437cf
krosengr4 <rosenkev4@gmail.com>
2024-02-26 09:17:45 -0600

    Coded seed.js, needs debugging. (Connec.&Models)

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,2 +1,21 @@
 //! This file contains code to put seed data (/profileSeeds.json) in db
 
+// Import db connection, Profile model, seed data, and cleanDB 
+const db = require('../config/connection');
+const { Profile } = require('../models');
+const profileSeeds = require('./profileSeeds.json');
+// const cleanDB = require('./cleanDB'); //?<--- Don't need yet
+
+// 
+db.once('open', async () => {
+    try {
+    //   await cleanDB('Profile', 'profiles'); //?<--- Don't need yet
+      
+      await Profile.create(profileSeeds);
+  
+      console.log('Success! Your data is seeded to the db!');
+      process.exit(0);
+    } catch (err) {
+      throw err;
+    }
+  });
