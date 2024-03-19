235ae7024325a5b2cf2b55f4dcb2a06e4cb8abb1
krosengr4 <rosenkev4@gmail.com>
2024-03-19 14:59:36 -0500

    Create homepage with link to 4 applications Daily planner, weather dashboard, and random pass generator

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,7 +11,44 @@ export default function homePage() {
         <div className="app-container">
             {/* <Link to="./applications/Planner">The Daily Planner</Link> */}
             {/* <plannerApp /> Planner */}
-            <Link to='./Planner'>Planner</Link>
+            {/* <Link to='https://krosengr4.github.io/WorkDayScheduler/'>Daily Planner</Link> */}
+            {/* <Link to='https://krosengr4.github.io/WorkDayScheduler/'>Daily Planner</Link> */}
+
+            <div className="main-div">
+        <h1 className="page-title">My Portfolio </h1>
+        <p className='description-tag'>Below are a few of the projects that I have created or helped create!</p>
+        <hr></hr>
+    <section className='projects-container'>
+        <a className='lower-project' href='https://krosengr4.github.io/WorkDayScheduler/'>
+            <img className='project-img' src='/Workday-scheduler.png' alt='Clickable image of my project'></img>
+            <div className='overlay4'>
+                <h3 className='small-project-label'>Workday Scheduler</h3>
+            </div>
+        </a>
+        
+        <a className='lower-project' href='https://krosengr4.github.io/Weather-Dashboard/'>
+            <img className='project-img' src='/Weather-Dashboard.png' alt='Clickable image of my project'></img>
+            <div className='overlay2'>
+                <h3 className='small-project-label'>Weather Dashboard</h3>
+            </div>
+        </a>
+
+        <a className='lower-project' href='https://krosengr4.github.io/Coding-Quiz/'>
+            <img className='project-img' src='/Code-Quiz.png' alt='Clickable image of my project'></img>
+            <div className='overlay3'>
+                <h3 className='small-project-label'>Coding Quiz</h3>
+            </div>
+        </a>
+
+        <a className='lower-project' href='https://krosengr4.github.io/Password-Generator/'>
+            <img className='project-img' src='/Password-Generator.png' alt='Clickable image of my project'></img>
+            <div className='overlay5'>
+                <h3 className='small-project-label'>Random Password Generator</h3>
+            </div>
+        </a>
+    </section>
+
+    </div>
         </div>
     )
 }
