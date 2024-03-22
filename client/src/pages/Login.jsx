44e377709f49c96836bc69731871e20aa5d9d6d0
krosengr4 <rosenkev4@gmail.com>
2024-03-22 16:02:46 -0500

    Worked on login page and user auth (needs debugging)

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,15 +2,93 @@
 
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
-
+import Auth from '../utils/auth';
 import '../app.css';
 
-export default function homePage() {
+const Login = (props) => {
+    const [formState, setFormState] = useState({ name: '', password: '' });
+    const [login, { error, data }] = useMutation(LOGIN_USER);
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  //submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+        const { data } = await login({
+            variables: { ...formState },
+        });
+
+        Auth.login(data.login.token);
+    } catch (e) {
+        console.error(e);
+    }
+
+    // clear form values
+    setFormState({
+        name: '',
+        password: '',
+    });
+  };
+
 
     return (
-        <div>
-            <h1>Login to your account!</h1>
-        </div>
-    )
-}
+        <main className='login-main'>
+            <div>
+                <h2 className='login-title'>Login to your account!</h2>
+                <div className='card-body'>
+                    {data ? (
+                        <p>
+                            You have been logged in! Head {' '}
+                            <Link to='/'>back to the homepage.</Link>
+                        </p>
+                    ) : (
+                        <form onSubmit={handleFormSubmit}>
+                            <input
+                                className='login-form'
+                                placeholder='Enter your name...'
+                                name='name'
+                                type='name'
+                                value={formState.name}
+                                onChange={handleChange}
+                            />
+                            <input
+                                className='login-form'
+                                placeholder='********'
+                                name='password'
+                                type='password'
+                                value={formState.password}
+                                onChange={handleChange}
+                            />
+                            <button className='login-submit' type='submit'>Login</button>    
+                        </form>
+                    )}
+
+                    {error && (
+                        <div className='error-msg'>
+                            {error.message}
+                        </div>
+                    )}
+                </div>
+            </div>
+        </main>
+    );
+    };
+
+export default Login;
+
+    // <div>
+    //     <h1>Login to your account!</h1>
+    // </div>
--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,20 +1,22 @@
 //! This file contains code for authentification
 
-// import
+// import decode to get the user's info from it
 import decode from 'jwt-decode';
 
+// create a new class to instantiate for a user
 class AuthService {
+    // get user data from JSON web token by decoding it
     getProfile() {
         return decode(this.getToken());
     }
 
+    // return `true` or `false` if token exists (does not verify if it's expired yet)
     loggedIn() {
-        // Checks if there is a saved token and it is valid
         const token = this.getToken();
-        return !!token && !this.isTokenExpired(token);
+        return token ? true : false;
     }
 
-    isTokenExpired(token) {
+    getToken() {
         // Retrieves the user token from localStorage
         return localStorage.getItem('id_token');
     }
@@ -22,7 +24,6 @@ class AuthService {
     login(idToken) {
         // Saves user token to localStorage
         localStorage.setItem('id_token', idToken);
-    
         window.location.assign('/');
       }
     
@@ -30,9 +31,8 @@ class AuthService {
         // Clear user token and profile data from localStorage
         localStorage.removeItem('id_token');
         // this will reload the page and reset the state of the application
-        window.location.assign('/');
+        window.location.reload();
       }
     }
-
 
 export default new AuthService();
